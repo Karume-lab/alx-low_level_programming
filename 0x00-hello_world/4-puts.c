2d2daf5545703319c611c3a02e4a3c0cea0a98e0
Karume-lab <danielkarume@outlook.com>
2022-12-08 09:19:21 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main: Entry point
- * @puts(): Writes a lin/string to the output.
- * Return: Always 0 (Success)
- */
-
+* main - Entry point
+* @puts() - Writes a lin/string to the output.
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
