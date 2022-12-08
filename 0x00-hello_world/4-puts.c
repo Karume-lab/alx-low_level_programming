2431b38423c6e87890082baf5200c5c289890645
Karume-lab <danielkarume@outlook.com>
2022-12-08 09:05:19 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include<stdio.h>
+/**
+ * main: Entry point
+ * @puts(): Writes a lin/string to the output.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
