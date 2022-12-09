350e8eaa14c496e164d75f13016ce425513ef26e
Karume-lab <danielkarume@outlook.com>
2022-12-09 09:17:59 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
+
