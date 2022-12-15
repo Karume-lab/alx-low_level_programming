bf970a84cdcbd7d7ff95c00cf3b28d0ef173795e
Karume-lab <danielkarume@outlook.com>
2022-12-14 18:38:21 -0800

    Federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  *
  *Return: Always 0
  */
-void jack_bauer(void)
+oid jack_bauer(void)
 {
 	int h, m, i, j, k, l;
 
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+nt main(void)
 {
 	jack_bauer();
 	return (0);
