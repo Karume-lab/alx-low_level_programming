29dbd8b4fa417adf264687108b34d101261cf717
Karume-lab <danielkarume@outlook.com>
2022-12-14 19:17:56 -0800

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,54 +1,11 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
+ *add - Prints the sum of two numbers
+ *@a: Parameter
+ *@b: Parameter
+ *Return: Always sum of a and b
  */
-void print_times_table(int n)
+int add(int a, int b)
 {
-	int a, b, c, d, e, f;
-
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b;
-				d = c / 100;
-				e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-					if (b < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					else
-						_putchar('\n');
-			}
-		}
-	}
+	return (a + b);
 }
