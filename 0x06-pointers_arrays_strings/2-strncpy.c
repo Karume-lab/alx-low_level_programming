13b32cae56e1f0a29205bb317dd376531a625538
Karume-lab <danielkarume@outlook.com>
2022-12-22 08:09:55 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strncpy - Copies a string
+ *@dest: Parameter
+ *@src: Parameter
+ *@n: Parameter
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
+
