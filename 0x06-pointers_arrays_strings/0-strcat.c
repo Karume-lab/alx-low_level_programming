2c4ceb916080f817a8cef45fa3aafcdc27bd273d
Karume-lab <danielkarume@outlook.com>
2022-12-22 07:56:27 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strcat - Entry Point
+*@dest: Parameter
+*@src: Parameter
+*
+*Return: destination string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + j)))
+	{
+		i++;
+		j++;
+	}
+	return (dest);
+}
+
