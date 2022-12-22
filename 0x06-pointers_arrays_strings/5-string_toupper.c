1dd17fefe8623f011eb7db94c89a9aee143eaddd
Karume-lab <danielkarume@outlook.com>
2022-12-22 08:23:37 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper - Changes lowercase to uppercase
+ *@s: Parameter
+ *Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && (*(s + i) <= 'z'))
+		*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
