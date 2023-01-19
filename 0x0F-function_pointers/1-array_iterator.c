26739ee155c875a9304e866605978c0311b79c4b
Karume-lab <danielkarume@outlook.com>
2023-01-19 00:36:19 -0800

    If you spend too much time thinking about a thing, you will never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if ( array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
