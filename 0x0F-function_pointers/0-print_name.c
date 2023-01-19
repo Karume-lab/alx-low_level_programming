d5f19a1a91172b59240dc28df673b596d4991620
Karume-lab <danielkarume@outlook.com>
2023-01-19 00:23:55 -0800

    What is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: first parameter
+ * @f: second parameter
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
