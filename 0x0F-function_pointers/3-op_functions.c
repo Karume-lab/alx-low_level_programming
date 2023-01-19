fc42a4eba00a9290650c184979d9b5b0dc37da83
Karume-lab <danielkarume@outlook.com>
2023-01-19 05:22:38 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,73 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add - finds the sum of a and b
- * @a: first argument
- * @b: second argument
- * Return: the sum
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
  */
-
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - finds the difference of a and b
- * @a: first argument
- * @b: second argument
- * Return: the difference
+ * op_sub - subtracts b from a
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
  */
-
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - finds the product of a and b
- * @a: first argument
- * @b: second argument
- * Return: Product
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
  */
-
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - finds the quotient of a and b
- * @a: first argument
- * @b: second argument
- * Return: The quotient
+ * op_div - divides a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer division of a by b
  */
-
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - finds the remainder after dividing a and b
- * @a: first argument
- * @b: second argument
- * Return: The remainder
+ * op_mod - find the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
  */
-
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
-
