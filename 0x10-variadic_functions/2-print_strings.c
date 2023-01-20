df49ae996a09f2dab5dad78d60b8d9ec6979e9af
Karume-lab <danielkarume@outlook.com>
2023-01-20 03:48:54 -0800

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,6 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *chr;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
@@ -20,9 +21,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", chr);
 		else
 			printf("(nil)");
-		if (i != (n - 1) && separator)
+		if (i != n - 1 && separator)
 			printf("%s", separator);
 	}
 	va_end(args);
 	printf("\n");
 }
+
