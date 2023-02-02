6025a62d3ee06e8756dc87c8b0bce7fb73b8bb7b
Karume-lab <danielkarume@outlook.com>
2023-02-02 05:24:58 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,18 +6,20 @@ size_t print_listint(const listint_t *h);
 /**
  * print_listint -  prints all the elements of a listint_t list.
  * @h: list to print
- *
  * Return: number of elements in the list
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h);
 {
-	size_t i = 0;
+	int i;
 
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
+
 	return (i);
 }
+
