d57e2d71a6616ddb99b3cfe35bacdf987c10e238
Karume <113604333+Karume-lab@users.noreply.github.com>
2023-02-02 16:32:25 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h);
  */
 size_t print_listint(const listint_t *h);
 {
-	size_t i;
+	size_t i = 0;
 
 	if (h == NULL)
-		return (0);
+		return (i);
 	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
