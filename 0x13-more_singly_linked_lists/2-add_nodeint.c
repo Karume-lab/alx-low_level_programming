a2c82dfcdecc86f5171a55af20ed6bf7096b8a79
Karume-lab <danielkarume@outlook.com>
2023-02-02 06:38:15 -0800

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,9 +10,10 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *ptr = (listint_t *)malloc(sizeof(listint_t));
+
 	if (head == NULL)
 		return (NULL);
-	listint_t *ptr = (listint_t *)malloc(sizeof(listint_t));
 
 	if (ptr == NULL)
 		return (NULL);
