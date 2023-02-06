fae135c61017e60b9090ff2152c349ed3c3686f4
Karume-lab <danielkarume@outlook.com>
2023-02-06 06:26:49 -0800

    Prototypes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdio.h>
+
+#include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 
 unsigned int binary_to_uint(const char *b);
@@ -10,4 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-#endif
+
+#endif 
+
