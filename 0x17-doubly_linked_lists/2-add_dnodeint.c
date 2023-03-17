77118a43781f62c6adb790e1d3f79106ab9e9cb1
Karume-lab <danielkarume@outlook.com>
2023-03-17 11:45:53 +0300

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,5 +21,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	*head = newNode;
 
+	if (newNode->next)
+		newNode->next->prev = newNode;
+
 	return (newNode);
 }
