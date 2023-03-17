9a6a4e1de96ed5e7d88e752d1027feac41dd71e3
Karume-lab <danielkarume@outlook.com>
2023-03-17 13:00:52 +0300

    Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,14 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint - Sums the elements
+ * @head: A pointer to the first node
+ * Return: The sum of the elements
+*/
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *trav = head;
 	int sum = 0;
-	
+
 	if (head == NULL)
 		return (sum);
 
