383b81e224cba334df24c12b9685ae9dff042322
Karume-lab <danielkarume@outlook.com>
2023-03-17 11:29:34 +0300

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - Finds the number of nodes
+ * @h: A pointer to the first node
+ * Return: The number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+    int num = 0;
+
+    if (h == NULL)
+        return (num);
+    while (h)
+    {
+        num++;
+        h = h->next;
+    }
+    return (num);
+}
