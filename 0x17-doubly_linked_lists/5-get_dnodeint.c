b763f80c0be2d760021892ff8aa513af6417a0cb
Karume-lab <danielkarume@outlook.com>
2023-03-17 12:48:18 +0300

    Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ *  get_dnodeint_at_index- Retrieve a value at given index
+ * @head: A pointer to the first node
+ * @index: The index of where the element is
+ * Return: A pointer to the node being looked for
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
