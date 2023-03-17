1b099ff7cf47adc0ff00cedcf6dbcaf94568916b
Karume-lab <danielkarume@outlook.com>
2023-03-17 17:59:53 +0300

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,42 +9,26 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *trav = *h;
-	dlistint_t *ptrNode = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *ptrNode = NULL;
 	unsigned int i;
 
-	if (ptrNode == NULL)
-		return (NULL);
-	ptrNode->n = n;
 	if (h == NULL)
-	{
-		free(ptrNode);
 		return (NULL);
-	}
 	if (*h == NULL || idx == 0)
-	{
-		free(ptrNode);
 		return (add_dnodeint(h, n));
-	}
 	for (i = 0; (i < idx - 1) && (trav); i++)
 		trav = trav->next;
 	if (trav == NULL)
-	{
-		free(ptrNode);
 		return (NULL);
-	}
 	if (trav->next == NULL)
-	{
-		free(ptrNode);
 		return (add_dnodeint_end(h, n));
-	}
-	else
-	{
-		trav->next->prev = ptrNode;
-		ptrNode->prev = trav;
-		ptrNode->next = trav->next;
-		trav->next = ptrNode;
-		return (ptrNode);
-	}
-
-	return (NULL);
+	ptrNode = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (ptrNode == NULL)
+		return (NULL);
+	ptrNode->n = n;
+	trav->next->prev = ptrNode;
+	ptrNode->prev = trav;
+	ptrNode->next = trav->next;
+	trav->next = ptrNode;
+	return (ptrNode);
 }
